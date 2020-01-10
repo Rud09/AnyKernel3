0c6f4ea8b62fa3c17e007d39c6b42e4a57c516aa
Rud09 <rudroy09@gmail.com>
2020-01-10 11:14:36 +0530

    fixed shellvariables

--- anykernel.sh
+++ anykernel.sh
@@ -15,8 +15,8 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+#block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+#is_slot_device=0;
 ramdisk_compression=auto;
 
 
